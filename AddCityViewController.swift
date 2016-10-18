5e2d63db5cd345dbee63c97abc3eb6278f6d9338
Ewa Czekalska <ewa.czekalska@gmail.com>
2016-10-18 09:41:40 +0100

    first commit from source tree

--- AddCityViewController.swift
+++ AddCityViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 protocol AddCityViewDelegate
 
 {
-    func resultOfBusinessLogic(valueSent:[String])
+    func resultOfBusinessLogic(valueSent:[String] , ratingSent:[Int]  )
 }
 
-class AddCityViewController: UIViewController {
+class AddCityViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    var ratingTypes = ["0", "1", "2","3","4","5"]
+    
+    var citiesCreatedInAddCityViewController : [String]! = []
+
+    var newCityAdded : String = String()
+    
+    var ratingsForAllCitiesArray : [Int] = []
+    
+    @IBOutlet var pickerView: UIPickerView!
     var delegate : AddCityViewDelegate?
     @IBOutlet weak var inputCityName: UITextField!
-    var oneCity : String = String()
-    var cities : [String]! = []
+
+    
+    var newRatingAdded=Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerView.delegate = self //so its able to control it
+        pickerView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
@@ -31,8 +44,12 @@ class AddCityViewController: UIViewController {
     
     @IBAction func saveButton1(_ sender: AnyObject) {
         
-        oneCity = inputCityName.text!
-        cities.append(oneCity)
+        //saving new city into a variable
+        newCityAdded = inputCityName.text!
+        
+        citiesCreatedInAddCityViewController.append(newCityAdded)
+        ratingsForAllCitiesArray.append(newRatingAdded)
+        
         performSegue(withIdentifier: "vc1", sender: sender)
     }
 
@@ -45,13 +62,33 @@ class AddCityViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "vc1")  {
         let destViewController : ViewController = segue.destination as! ViewController
-            destViewController.resultOfBusinessLogic(valueSent: cities)
-        destViewController.labelText=inputCityName.text!
+            destViewController.resultOfBusinessLogic(valueSent: citiesCreatedInAddCityViewController, ratingSent: ratingsForAllCitiesArray)
+       // destViewController.labelText=inputCityName.text!
+          //  destViewController.ratingAddedFromAddCityViewController.append(newRatingAdded)
+
+            print("The rating value is\(destViewController.ratingAddedFromAddCityViewController)")
         }
     }
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ratingTypes[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return ratingTypes.count
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        newRatingAdded = row
+       // newRatingAdded = row + 1
+    }
 }
